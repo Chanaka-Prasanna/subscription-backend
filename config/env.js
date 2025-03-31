c5d99023261267a10a8a58b51f7c1c4e72445f4e
Chanaka-Prasanna <chanakapinfo@gmail.com>
2025-03-31 23:50:04 +0530

    First commit

---
+++ config/env.js
@@ -0,0 +1,7 @@
+import { config } from "dotenv";
+
+config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
+
+//  easy to switch between development and priduction environments
+//  by changing the NODE_ENV variable in the .env file. So we do't need to override it
+export const { PORT, NODE_ENV } = process.env;
